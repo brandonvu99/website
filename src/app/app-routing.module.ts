df948fb5451bc1f75d8a6f91a803b8c75fff1c82
Brandon Vu <bvu32@gatech.edu>
2021-10-20 16:02:58 -0400

    added every page as a skeleton

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './components/pages/about/about.component';
+import { ContactComponent } from './components/pages/contact/contact.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { ProjectsComponent } from './components/pages/projects/projects.component';
+import { ResumeComponent } from './components/pages/resume/resume.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'', component:HomeComponent },
+  { path:'about', component:AboutComponent },
+  { path:'projects', component:ProjectsComponent },
+  { path:'resume', component:ResumeComponent },
+  { path:'home-server', component:AboutComponent },
+  { path:'contact', component:ContactComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { AboutComponent } from './components/pages/about/about.component';
+import { ProjectsComponent } from './components/pages/projects/projects.component';
+import { ResumeComponent } from './components/pages/resume/resume.component';
+import { HomeServerComponent } from './components/pages/home-server/home-server.component';
+import { ContactComponent } from './components/pages/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ProjectsComponent,
+    ResumeComponent,
+    HomeServerComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
